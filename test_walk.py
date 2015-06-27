61705142d4d3e43fedf8e519d07d56c508c208b7
Peter Williams <pwil3058@gmail.com>
2015-06-27 16:10:05 +1000

    measure: add mechanism to save/load snapshot to/from disk and measure
    Results:
    
                                bytes   time (sec)      Mb/sec
    with no prior snapshot: 34722749119 130.449277   266.178164552
    with prior snapshot   : 34837889892   5.87224   5932.64067749
    
    Should be noted that there was only very small changes between snapshots.
    
    Snapshot size: 115102691 for 373067 files gives 308 bytes per file.
    
    Also should be noted that no writing of bobs has occurred yet.
    
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- test_walk.py
+++ test_walk.py
@@ -28,6 +28,8 @@ parser = argparse.ArgumentParser(description="Test os.walk().")
 parser.add_argument("dir_path", metavar="dir", type=str, nargs="?", default=".", help="the path of the directory to be walked")
 parser.add_argument("--xfile", action="append", required=False, help="exclude files matching this pattern")
 parser.add_argument("--xdir", action="append", required=False, help="exclude directories matching this pattern")
+parser.add_argument("--snapshot", required=False, help="name of file in which to save the snapshot data")
+parser.add_argument("--prior", required=False, help="name of file containing prior snapshot data")
 
 args = parser.parse_args()
 
@@ -50,7 +52,54 @@ def is_excluded_dir(dir_path):
 # The file has gone away
 FORGIVEABLE_ERRNOS = frozenset((errno.ENOENT, errno.ENXIO))
 
-file_data = {}
+class Directory(object):
+    def __init__(self, dir_stats=None):
+        self.dir_stats = dir_stats
+        self.subdirs = {}
+        self.files = {}
+    def __str__(self):
+        string = "\n".join([str(self.dir_stats), str(self.files)])
+        for name, subdir in self.subdirs.items():
+            string += "\n{0}:{1}\n".format(name, str(subdir))
+        return string
+    def _add_subdir(self, path_parts, dir_stats=None):
+        name = path_parts[0]
+        if len(path_parts) == 1:
+            self.subdirs[name] = Directory(dir_stats)
+            return self.subdirs[name]
+        else:
+            if name not in self.subdirs:
+                self.subdirs[name] = Directory()
+            return self.subdirs[name]._add_subdir(path_parts[1:], dir_stats)
+    def add_subdir(self, dir_path, dir_stats=None):
+        return self._add_subdir(dir_path.strip(os.sep).split(os.sep), dir_stats)
+    def _find_dir(self, dirpath_parts):
+        if not dirpath_parts:
+            return self
+        elif dirpath_parts[0] in self.subdirs:
+            return self.subdirs[dirpath_parts[0]]._find_dir(dirpath_parts[1:])
+        else:
+            return None
+    def find_dir(self, dirpath):
+        if not dirpath:
+            return self
+        return self._find_dir(dirpath.strip(os.sep).split(os.sep))
+
+base_dir = Directory()
+
+if args.prior:
+    import cPickle
+    fobj = open(args.prior, 'rb')
+    try:
+        prior_snapshot = cPickle.load(fobj)
+    except Exception:
+        # Just in case higher level code catches and handles
+        prior_snapshot = None
+        raise
+    finally:
+        fobj.close()
+else:
+    prior_snapshot = Directory()
 
 content_count = 0
 
@@ -59,6 +108,9 @@ start = time.clock()
 for dir_path, subdir_paths, file_names in os.walk(args.dir_path, followlinks=False):
     if is_excluded_dir(dir_path):
         continue
+    file_data = base_dir.add_subdir(dir_path, os.lstat(dir_path)).files
+    prior_dir = prior_snapshot.find_dir(dir_path)
+    prior_file_data = {} if prior_dir is None else prior_dir.files
     for file_name in file_names:
         if is_excluded_file(file_name):
             continue
@@ -73,17 +125,20 @@ for dir_path, subdir_paths, file_names in os.walk(args.dir_path, followlinks=Fal
                 continue # it's gone away so we skip it
             raise edata # something we can't handle so throw the towel in
         if stat.S_ISREG(file_stats.st_mode):
-            try:
-                content = open(file_path, "r").read()
-                #content = ""
-            except OSError as edata:
-                # race condition
-                if edata.errno in FORGIVEABLE_ERRNOS:
-                    continue  # it's gone away so we skip it
-                raise edata # something we can't handle so throw the towel in
-            hex_digest = hashlib.sha1(content).hexdigest()
+            prior_file = prior_file_data.get(file_name, None) if prior_file_data else None
+            if prior_file and (prior_file[0].st_size == file_stats.st_size) and (prior_file[0].st_mtime == file_stats.st_mtime):
+                hex_digest = prior_file[1]
+            else:
+                try:
+                    content = open(file_path, "r").read()
+                except OSError as edata:
+                    # race condition
+                    if edata.errno in FORGIVEABLE_ERRNOS:
+                        continue  # it's gone away so we skip it
+                    raise edata # something we can't handle so throw the towel in
+                hex_digest = hashlib.sha1(content).hexdigest()
             content_count += file_stats.st_size
-            file_data[file_path] = (False, file_stats, hex_digest)
+            file_data[file_name] = (file_stats, hex_digest)
         elif stat.S_ISLNK(file_stats.st_mode):
             try:
                 target_file_path = os.readlink(file_path)
@@ -96,7 +151,7 @@ for dir_path, subdir_paths, file_names in os.walk(args.dir_path, followlinks=Fal
                 # no sense storing broken links as they will cause problems at restore
                 sys.stderr.write("{0} -> {1} symbolic link is broken.  Skipping.\n".format(file_path, target_file_path))
                 continue
-            file_data[file_path] = (True, file_stats, target_file_path)
+            file_data[file_name] = (file_stats, target_file_path)
         else:
             continue
     excluded_subdir_paths = [subdir_path for subdir_path in subdir_paths if is_excluded_dir(subdir_path)]
@@ -109,3 +164,11 @@ stop = time.clock()
 etime = stop - start
 
 print "DONE", len(file_data), content_count, etime, content_count / etime / 1000000, len(file_data) / etime
+
+if args.snapshot:
+    import cPickle
+    fobj = open(args.snapshot, 'wb', stat.S_IRUSR|stat.S_IRGRP)
+    try:
+        cPickle.dump(base_dir, fobj)
+    finally:
+        fobj.close()
