2c95279268302ca0155c98b0585dce918bfd9bf5
Peter Williams <pwil3058@gmail.com>
2015-06-30 21:35:25 +1000

    config: read profile configuration from configuration files
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- epygibus_pkg/config.py
+++ epygibus_pkg/config.py
@@ -13,13 +13,32 @@
 ### along with this program; if not, write to the Free Software
 ### Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-class Profile(object):
-    repo_name = None
-    snapshot_dir_path = "test"
-    includes = ["~/Downloads", "~/Dropbox/bedienung_infinity_2009.pdf"]
-    exclude_dir_cres = list()
-    exclude_file_cres = list()
-    skip_broken_soft_links = False
+import os
+import sys
+import collections
+import fnmatch
 
-def read_profile_spec(profile_name):
-    return Profile()
+from . import APP_NAME
+
+_CONFIG_DIR_PATH = os.path.expanduser(os.path.join("~", ".config", APP_NAME + ".d"))
+_REPOS_DIR_PATH = os.path.join(_CONFIG_DIR_PATH, "repos")
+_PROFILES_DIR_PATH = os.path.join(_CONFIG_DIR_PATH, "profiles")
+
+if not os.path.exists(_CONFIG_DIR_PATH):
+    os.makedirs(_CONFIG_DIR_PATH)
+    os.mkdir(_REPOS_DIR_PATH)
+    os.mkdir(_PROFILES_DIR_PATH)
+
+_profiles_config_lines = lambda pname: open(os.path.join(_PROFILES_DIR_PATH, pname, "config"), "r").readlines()
+_includes_file_lines = lambda pname: open(os.path.join(_PROFILES_DIR_PATH, pname, "includes"), "r").readlines()
+_exclude_dir_lines = lambda pname: open(os.path.join(_PROFILES_DIR_PATH, pname, "exclude_dirs"), "r").readlines()
+_exclude_file_lines = lambda pname: open(os.path.join(_PROFILES_DIR_PATH, pname, "exclude_files"), "r").readlines()
+
+Profile = collections.namedtuple("Profile", ["repo_name", "snapshot_dir_path", "includes", "exclude_dir_cres", "exclude_file_cres", "skip_broken_soft_links"])
+
+def read_profile_spec(profile_name, stderr=sys.stderr):
+    repo, p_dir_path, skip = [l.rstrip() for l in _profiles_config_lines(profile_name)]
+    includes = [os.path.abspath(os.path.expanduser(f.rstrip())) for f in _includes_file_lines(profile_name)]
+    dir_excludes = [fnmatch.translate(os.path.expanduser(glob.rstrip())) for glob in _exclude_dir_lines(profile_name)]
+    file_excludes = [fnmatch.translate(os.path.expanduser(glob.rstrip())) for glob in _exclude_file_lines(profile_name)]
+    return Profile(repo, p_dir_path, includes, dir_excludes, file_excludes, eval(skip))
