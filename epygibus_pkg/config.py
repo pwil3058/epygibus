6f35b130d1deda2730717c7fdff0b7cdfa0e4c8f
Peter Williams <pwil3058@gmail.com>
2016-05-18 14:29:02 +1000

    Add mechanism for writing/deleting profile specs and tidy up config code
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- epygibus_pkg/config.py
+++ epygibus_pkg/config.py
@@ -20,7 +20,17 @@ import fnmatch
 
 from . import APP_NAME
 
-_CONFIG_DIR_PATH = os.path.expanduser(os.path.join("~", ".config", APP_NAME + ".d"))
+APP_NAME_D = APP_NAME + ".d"
+
+class Error(Exception):
+    def __init__(self, *args, **kargs):
+        self.args = args
+        self.kargs = kargs
+
+class ErrorRepoSpecExists(Error):
+    pass
+
+_CONFIG_DIR_PATH = os.path.expanduser(os.path.join("~", ".config", APP_NAME_D))
 _REPOS_DIR_PATH = os.path.join(_CONFIG_DIR_PATH, "repos")
 _PROFILES_DIR_PATH = os.path.join(_CONFIG_DIR_PATH, "profiles")
 
@@ -29,27 +39,57 @@ if not os.path.exists(_CONFIG_DIR_PATH):
     os.mkdir(_REPOS_DIR_PATH)
     os.mkdir(_PROFILES_DIR_PATH)
 
-_profiles_config_lines = lambda pname: open(os.path.join(_PROFILES_DIR_PATH, pname, "config"), "r").readlines()
-_includes_file_lines = lambda pname: open(os.path.join(_PROFILES_DIR_PATH, pname, "includes"), "r").readlines()
-_exclude_dir_lines = lambda pname: open(os.path.join(_PROFILES_DIR_PATH, pname, "exclude_dirs"), "r").readlines()
-_exclude_file_lines = lambda pname: open(os.path.join(_PROFILES_DIR_PATH, pname, "exclude_files"), "r").readlines()
+_repo_file_path = lambda rname: os.path.join(_REPOS_DIR_PATH, rname)
+
+_profile_dir_path = lambda pname: os.path.join(_PROFILES_DIR_PATH, pname)
+_profile_config_path = lambda pname: os.path.join(_profile_dir_path(pname), "config")
+_profile_includes_path = lambda pname: os.path.join(_profile_dir_path(pname), "includes")
+_profile_exclude_dirs_path = lambda pname: os.path.join(_profile_dir_path(pname), "exclude_dirs")
+_profile_exclude_files_path = lambda pname: os.path.join(_profile_dir_path(pname), "exclude_files")
+
+_profile_config_lines = lambda pname: open(_profile_config_path(pname), "r").readlines()
+_includes_file_lines = lambda pname: open(_profile_includes_path(pname), "r").readlines()
+_exclude_dir_lines = lambda pname: open(_profile_exclude_dirs_path(pname), "r").readlines()
+_exclude_file_lines = lambda pname: open(_profile_exclude_files_path(pname), "r").readlines()
 
 Profile = collections.namedtuple("Profile", ["repo_name", "snapshot_dir_path", "includes", "exclude_dir_cres", "exclude_file_cres", "skip_broken_soft_links"])
 
 def read_profile_spec(profile_name, stderr=sys.stderr):
-    repo, p_dir_path, skip = [l.rstrip() for l in _profiles_config_lines(profile_name)]
+    repo, p_dir_path, skip = [l.rstrip() for l in _profile_config_lines(profile_name)]
     includes = [os.path.abspath(os.path.expanduser(f.rstrip())) for f in _includes_file_lines(profile_name)]
     dir_excludes = [fnmatch.translate(os.path.expanduser(glob.rstrip())) for glob in _exclude_dir_lines(profile_name)]
     file_excludes = [fnmatch.translate(os.path.expanduser(glob.rstrip())) for glob in _exclude_file_lines(profile_name)]
     return Profile(repo, p_dir_path, includes, dir_excludes, file_excludes, eval(skip))
 
+def write_profile_spec(profile_name, in_dir_path, repo_name, includes, exclude_dirs, exclude_files, skip_broken_sl=True):
+    base_dir_path = os.path.join(os.path.abspath(in_dir_path), APP_NAME_D, "snapshots", os.environ["HOSTNAME"], os.environ["USER"], profile_name)
+    os.mkdir(_profile_dir_path(profile_name))
+    open(_profile_config_path(profile_name), "w").writelines([p + os.linesep for p in[repo_name, base_dir_path, str(skip_broken_sl)]])
+    open(_profile_includes_path(profile_name), "w").writelines(includes)
+    open(_profile_exclude_dirs_path(profile_name), "w").writelines(exclude_dirs)
+    open(_profile_exclude_files_path(profile_name), "w").writelines(exclude_files)
+    return base_dir_path
+
+def delete_profile_spec(profile_name):
+    os.remove(_profile_config_path(profile_name))
+    os.remove(_profile_includes_path(profile_name))
+    os.remove(_profile_exclude_dirs_path(profile_name))
+    os.remove(_profile_exclude_files_path(profile_name))
+    os.rmdir(_profile_dir_path(profile_name))
+
 Repo = collections.namedtuple("Repo", ["base_dir_path"])
 
 def read_repo_spec(repo_name):
-    base_dir_path = open(os.path.join(_REPOS_DIR_PATH, repo_name)).read().rstrip()
+    base_dir_path = open(_repo_file_path(repo_name)).read().rstrip()
     return Repo(base_dir_path)
 
 def write_repo_spec(repo_name, in_dir_path):
-    base_dir_path = os.path.join(os.path.abspath(in_dir_path), "epygibus", "blobs", repo_name)
-    open(os.path.join(_REPOS_DIR_PATH, repo_name), "w").write(base_dir_path)
+    base_dir_path = os.path.join(os.path.abspath(in_dir_path), APP_NAME_D, "blobs", repo_name)
+    cf_path = _repo_file_path(repo_name)
+    if os.path.exists(cf_path):
+        raise ErrorRepoSpecExists(name=repo_name)
+    open(cf_path, "w").write(base_dir_path)
     return base_dir_path
+
+def delete_repo_spec(repo_name):
+    os.remove(_repo_file_path(repo_name))
