f78ebaad6bcdaff9ad829d8f3a82087ecdd521af
Peter Williams <pwil3058@gmail.com>
2016-06-19 15:53:37 +1000

    research: measure different combos of digest and copy mechanisms
    Python 2:
    
    hex_digest_and_copyfileobj : hex_digest_read -> (64.29999999981374, 115106, 6468895416)
    hex_digest_and_copyfileobj : hex_digest_mmap -> (70.45999999996275, 115107, 6468902834)
    hex_digest_and_copyfileobj : hex_digest_read_chunks -> (70.87999999988824, 115107, 6468905844)
    hex_digest_and_read_write : hex_digest_read -> (75.6699999999255, 115107, 6468908902)
    hex_digest_and_read_write : hex_digest_mmap -> (73.6699999999255, 115108, 6468956258)
    hex_digest_and_read_write : hex_digest_read_chunks -> (74.5, 115109, 6468964106)
    
    Python 3:
    
    hex_digest_and_copyfileobj : hex_digest_read -> (63.45999999996275, 115102, 6468810054)
    hex_digest_and_copyfileobj : hex_digest_mmap -> (70.48000000044703, 115102, 6468812634)
    hex_digest_and_copyfileobj : hex_digest_read_chunks -> (71.55999999959022, 115102, 6468816316)
    hex_digest_and_read_write : hex_digest_read -> (72.79000000003725, 115102, 6468819554)
    hex_digest_and_read_write : hex_digest_mmap -> (74.00999999977648, 115103, 6468849132)
    hex_digest_and_read_write : hex_digest_read_chunks -> (75.44000000040978, 115104, 6468856881)
    
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- research/test_digest_and_copy_dir.py
+++ research/test_digest_and_copy_dir.py
@@ -24,34 +24,53 @@ import time
 import argparse
 import mmap
 import io
+import shutil
 
-parser = argparse.ArgumentParser(description="Test various options for getting file sha1 digest for a directory.")
-parser.add_argument("dir_path", metavar="directory", type=str, action="store", help="the path of the direcory whose files' digests are to be calculated")
+parser = argparse.ArgumentParser(description="Test various options for getting file sha1 digest for a directory and copying all files.")
+parser.add_argument("dir_path", metavar="directory", type=str, action="store", help="the path of the direcory whose files are to be processed")
 
 args = parser.parse_args()
 
-def hex_digest_mmap(file_path):
-    f = io.open(file_path, "rb")
+TGT_FILE = "TMPFILE"
+EMPTY_FILE_HASH = hashlib.sha1(b"").hexdigest()
+
+def hex_digest_mmap(fobj):
     try:
-        m = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+        m = mmap.mmap(fobj.fileno(), 0, access=mmap.ACCESS_READ)
         hd = hashlib.sha1(m).hexdigest()
         m.close()
     except ValueError:
-        hd = hashlib.sha1(b"").hexdigest()
-    f.close()
+        hd = EMPTY_FILE_HASH
     return hd
 
-def hex_digest_read(file_path):
-    return hashlib.sha1(io.open(file_path, "rb").read()).hexdigest()
+def hex_digest_read(fobj):
+    hd = hashlib.sha1(fobj.read()).hexdigest()
+    fobj.seek(0)
+    return hd
 
-def hex_digest_read_chunks(file_path):
+def hex_digest_read_chunks(fobj):
     h = hashlib.sha1()
-    with io.open(file_path, "rb") as f:
-        for x in iter(lambda: f.read(10000000), b""):
-            h.update(x)
+    for x in iter(lambda: fobj.read(10000000), b""):
+        h.update(x)
+    fobj.seek(0)
     return h.hexdigest()
 
-def walk_the_tree(directory_path, functn, iterations=5):
+def hex_digest_and_copyfileobj(file_path, hd_fun):
+    # two levels of with to mic what repo code will need
+    with io.open(file_path, "rb") as f_in:
+        hd = hd_fun(f_in)
+        with io.open(TGT_FILE, "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    return hd
+
+def hex_digest_and_read_write(file_path, hd_fun):
+    with io.open(file_path, "rb") as f_in:
+        hd = hd_fun(f_in)
+        with io.open(TGT_FILE, "wb") as f_out:
+            f_out.write(f_in.read())
+    return hd
+
+def walk_the_tree(directory_path, functn_0, functn_1):
     count = 0
     total_bytes = 0
     abs_dir_path = os.path.abspath(os.path.expanduser(directory_path))
@@ -62,9 +81,10 @@ def walk_the_tree(directory_path, functn, iterations=5):
             if not os.path.isfile(file_path): continue
             total_bytes += os.path.getsize(file_path)
             count += 1
-            for i in range(iterations):
-                functn(file_path)
+            functn_0(file_path, functn_1)
+            assert os.path.getsize(file_path) == os.path.getsize(TGT_FILE)
     return (os.times()[4] - start, count, total_bytes)
 
-for name in ["hex_digest_read", "hex_digest_mmap", "hex_digest_read_chunks"]:
-    print (name, walk_the_tree(args.dir_path, eval(name)))
+for wname in ["hex_digest_and_copyfileobj", "hex_digest_and_read_write"]:
+    for name in ["hex_digest_read", "hex_digest_mmap", "hex_digest_read_chunks"]:
+        print (wname, ":", name, "->", walk_the_tree(args.dir_path, eval(wname), eval(name)))
